3e11106055727805689f34b85b96825db5af3725
aljabricolony <aljabricolony@outlook.com>
2022-03-11 02:26:35 +0000

    Update JenkinsCITest2.java

--- JenkinsCITest2.java
+++ JenkinsCITest2.java
@@ -3,6 +3,6 @@ public class JenkinsCITest2
    public static void main(String args[])
    {
       System.out.println("Enabled CI on this project by Poll SCM");
- 
+      System.out.println("Updating code to see a new automatic build - started by SCM change");
    }
 }
