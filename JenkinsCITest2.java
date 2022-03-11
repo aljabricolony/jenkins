3ba655b9612557db6ba92de073d69c2f9ae7186c
aljabricolony <aljabricolony@outlook.com>
2022-03-11 02:11:43 +0000

    Update JenkinsCITest2.java

--- JenkinsCITest2.java
+++ JenkinsCITest2.java
@@ -2,6 +2,7 @@ public class JenkinsCITest2
 {
    public static void main(String args[])
    {
+      System.out.println("Enable CI on this project by GitHub Hook");
       System.out.println("Hello World from Code 2");
    }
 }
